8323e385f6abd58443fcc6c5dceb5db999c776a0
hermann90 <hermannchefouet@gmail.com>
2021-11-13 19:54:47 +0400

    CICD Utrains Pipeline Initial version

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -1,4 +1,11 @@
+
 resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "pipeline-artifacts-utrains"
   acl    = "private"
+
+  force_destroy = true
+
+    versioning {
+        enabled = true
+    }
 } 
